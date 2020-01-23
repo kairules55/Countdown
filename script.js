10450d7ebe62ceb93e80a8f8fdb3d0ca4ab72958
Krishana <krishnakhatri264@gmail.com>
2020-01-24 01:18:32 +0530

    Basic Countdown Structure

---
+++ script.js
@@ -0,0 +1,14 @@
+var count = 11;
+var displayTimer = document.getElementById('display');
+
+function timer() {
+    if (count == 0) {
+        displayTimer.innerText = "Timeout";
+        clearInterval(id);
+        return;
+    }
+    displayTimer.innerText = count;
+    count--;
+}
+
+var id = setInterval(timer, 1000);
